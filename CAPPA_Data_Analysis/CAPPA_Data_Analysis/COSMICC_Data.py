93193e962d1244fc56cb153f9792e231fecc77a4
Robert Sheehan <robertnsheehan@gmail.com>
2019-07-03 16:42:16 +0100

    Played with COSMICC NS plots. They can be normalised, but it doesn't change much.

--- CAPPA_Data_Analysis/CAPPA_Data_Analysis/COSMICC_Data.py
+++ CAPPA_Data_Analysis/CAPPA_Data_Analysis/COSMICC_Data.py
@@ -13,7 +13,7 @@ import Plotting
 
 MOD_NAME_STR = "COSMICC_Data" # use this in exception handling messages
 
-def Nanostick_Spectrum(num, loud = False):
+def Nanostick_Spectrum(num, normalise = False, loud = False):
 
     # plot the measured transmission spectra for Simone's Nanostick devices
     # R. Sheehan 2 - 7 - 2019
@@ -36,16 +36,28 @@ def Nanostick_Spectrum(num, loud = False):
                 data = np.loadtxt(filename, unpack = True)
 
                 if data is not None:
+
+                    # normalise the data so that max value is unity
+                    if normalise:
+                        
+                        scal_fac = -1.0*np.min(data[1])
+
+                        if loud: print("Scale_factor:",scal_fac)
+
+                        for i in range(0, len(data[1]),1):
+                            data[1][i] = 1.0+(data[1][i] / scal_fac)
+
                     # plot the data in the file
                     arguments = Plotting.plot_arg_single()
-                    arguments.y_label = 'Power (dBm)'
+                    arguments.y_label = 'Power (dBm)' if normalise == False else 'Transmission (a.u.)'
                     arguments.x_label = 'Wavelength (nm)'
                     arguments.marker = 'r-'
                     #arguments.plt_range = [1520, 1540, -76, -46]
-                    arguments.loud = True
+                    arguments.plt_range = [1520, 1540, 0, 0.4]
+                    arguments.loud = loud
                     #arguments.fig_name = filename.replace('.dat','')
 
-                    Plotting.plot_single_curve(data[0], -1.0*data[1]-45, arguments)
+                    Plotting.plot_single_curve(data[0], data[1], arguments)
 
                     del data
                 else:
@@ -61,7 +73,7 @@ def Nanostick_Spectrum(num, loud = False):
         print(ERR_STATEMENT);
         print(e);
 
-def Nanostick_Spectra(loud = False):
+def Nanostick_Spectra(normalise = False, loud = False):
 
     # plot the measured transmission spectra for Simone's Nanostick devices
     # R. Sheehan 2 - 7 - 2019
@@ -84,19 +96,26 @@ def Nanostick_Spectra(loud = False):
                 if glob.glob(filename):
                     print(filename,"exists")
                     data = np.loadtxt(filename, unpack = True)
-                    hv_data.append(data); mrk_lst.append(Plotting.labs_lins[i%len(Plotting.labs_lins)]); lab_lst.append(filename.replace('.dat',''))
+                    if normalise:
+                        scal_fac = -1.0*np.min(data[1])
+                        for j in range(0, len(data[1]), 1):
+                            data[1][j] = 1.0+(data[1][j] / scal_fac)
+                    hv_data.append(data); 
+                    mrk_lst.append(Plotting.labs_lins[i%len(Plotting.labs_lins)]); 
+                    lab_lst.append(filename.replace('.dat',''))
 
             if hv_data is not None:
                 # plot the data in the file
                 arguments = Plotting.plot_arg_multiple()
 
-                arguments.y_label = 'Power (dBm)'
+                arguments.y_label = 'Power (dBm)' if normalise == False else 'Transmission (a.u.)'
                 arguments.x_label = 'Wavelength (nm)'
                 arguments.mrk_list = mrk_lst
                 arguments.crv_lab_list = lab_lst
-                arguments.plt_range = [1520, 1540, -76, -46]
-                arguments.loud = True
-                arguments.fig_name = 'Combined_Nanostick_Spectra'
+                #arguments.plt_range = [1520, 1526, -76, -56]
+                arguments.plt_range = [1520, 1540, 0.0, 0.4]
+                arguments.loud = loud
+                #arguments.fig_name = 'Combined_Nanostick_Spectra_Zoom_1'
 
                 Plotting.plot_multiple_curves(hv_data, arguments)
 
@@ -139,16 +158,16 @@ def Nanostick_Peak_Analysis(num, loud = False):
                 if data is not None:
                     from scipy.signal import find_peaks, peak_prominences, peak_widths
 
-                    peaks, heights = find_peaks(-1.0*data[1]-45, height = 5)
-                    prominences = peak_prominences(-1.0*data[1]-45, peaks)[0] # compute peak prominences
-                    widths = peak_widths(-1.0*data[1]-45, peaks, rel_height=0.5)[0] # compute peak FWHM
+                    peaks, heights = find_peaks(-1.0*data[1], height = 5)
+                    prominences = peak_prominences(-1.0*data[1], peaks)[0] # compute peak prominences
+                    widths = peak_widths(-1.0*data[1], peaks, rel_height=0.5)[0] # compute peak FWHM
 
                     #print(widths)
 
                     for i in range(0, len(peaks), 1):
                         if prominences[i] > 2:
-                            Q = data[0][ peaks[i] ]/widths[i]
-                            print("Peak:",data[0][ peaks[i] ], heights['peak_heights'][i], prominences[i], widths[i], Q)
+                            Q = 500.0*data[0][ peaks[i] ]/widths[i]
+                            print("Peak:",data[0][ peaks[i] ], heights['peak_heights'][i], prominences[i], widths[i]/500.0, Q)
 
                     del data
                 else:
@@ -168,6 +187,27 @@ def peak_search_example():
     # implementation of the peak search example in 
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.peak_widths.html
 
+    from scipy.signal import chirp, find_peaks, peak_widths
 
+    x = np.linspace(0, 6 * np.pi, 1000)
+    x = np.sin(x) + 0.6 * np.sin(2.6 * x) # Create a test signal with two overlayed harmonics
 
-    pass
+    # Find all peaks and calculate their widths at the relative height of 0.5 
+    # (contour line at half the prominence height) and 1 (at the lowest contour line at full prominence height)
+
+    peaks, _ = find_peaks(x)
+    results_half = peak_widths(x, peaks, rel_height=0.5)
+    results_half[0]  # widths
+
+    print(results_half[0])
+
+    results_full = peak_widths(x, peaks, rel_height=1)
+    results_full[0]  # widths
+
+    # Plot signal, peaks and contour lines at which the widths where calculated
+
+    plt.plot(x)
+    plt.plot(peaks, x[peaks], "x")
+    plt.hlines(*results_half[1:], color="C2")
+    #plt.hlines(*results_full[1:], color="C3")
+    plt.show()
